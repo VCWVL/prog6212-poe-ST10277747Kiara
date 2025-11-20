90a74710d01c743e266681ade3df7c4e9c6b6e8d
ST10277747Kiara <164771350+ST10277747Kiara@users.noreply.github.com>
2025-11-20 16:45:15 +0200

    Comit 2: this has my accounts controller that holds the logic for my suers login table and then also my db context that is used for claims , documents and users

---
+++ Data/CMCSPART3.Data.cs
@@ -0,0 +1,22 @@
+﻿using CMCSPART3.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace CMCSPART3.Data
+{
+    public class CMCSPART3DbContext : DbContext
+    {
+        public CMCSPART3DbContext(DbContextOptions<CMCSPART3DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Claim> Claims { get; set; }
+
+        // If you have a Document model, use this:
+        // public DbSet<Document> Documents { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
